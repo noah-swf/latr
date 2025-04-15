a788c4c4f1227597a48f62bd04798215abd1b510
noah-swf <kingsleagueyt@gmail.com>
2025-04-15 23:18:57 +0200

    Refactor pagination view to update  layout and button labels to German

--- resources/views/vendor/pagination/simple-tailwind.blade.php
+++ resources/views/vendor/pagination/simple-tailwind.blade.php
@@ -1,24 +1,24 @@
 @if ($paginator->hasPages())
-    <nav role="navigation" aria-label="Pagination Navigation" class="flex justify-between">
+    <nav role="navigation" aria-label="Pagination Navigation" class="flex justify-end mr-0">
         {{-- Previous Page Link --}}
         @if ($paginator->onFirstPage())
-            <span class="relative inline-flex items-center px-4 py-2 text-sm font-medium text-gray-500 bg-white border border-gray-300 cursor-default leading-5 rounded-md dark:text-gray-600 dark:bg-gray-800 dark:border-gray-600">
-                {!! __('pagination.previous') !!}
+            <span class="relative inline-flex items-center px-4 py-2 text-sm font-medium text-gray-500 cursor-default leading-5">
+                {!! __('Zurück') !!}
             </span>
         @else
-            <a href="{{ $paginator->previousPageUrl() }}" rel="prev" class="relative inline-flex items-center px-4 py-2 text-sm font-medium text-gray-700 bg-white border border-gray-300 leading-5 rounded-md hover:text-gray-500 focus:outline-none focus:ring ring-gray-300 focus:border-blue-300 active:bg-gray-100 active:text-gray-700 transition ease-in-out duration-150 dark:bg-gray-800 dark:border-gray-600 dark:text-gray-300 dark:focus:border-blue-700 dark:active:bg-gray-700 dark:active:text-gray-300">
-                {!! __('pagination.previous') !!}
+            <a href="{{ $paginator->previousPageUrl() }}" rel="prev" class="relative inline-flex items-center px-4 py-2 text-sm font-medium text-black leading-5 hover:text-gray-500 focus:outline-none focus:ring ring-gray-300 focus:border-blue-300  active:text-gray-700 transition ease-in-out duration-150">
+                {!! __('Zurück') !!}
             </a>
         @endif
-
+        <div class="py-2">|</div>
         {{-- Next Page Link --}}
         @if ($paginator->hasMorePages())
-            <a href="{{ $paginator->nextPageUrl() }}" rel="next" class="relative inline-flex items-center px-4 py-2 text-sm font-medium text-gray-700 bg-white border border-gray-300 leading-5 rounded-md hover:text-gray-500 focus:outline-none focus:ring ring-gray-300 focus:border-blue-300 active:bg-gray-100 active:text-gray-700 transition ease-in-out duration-150 dark:bg-gray-800 dark:border-gray-600 dark:text-gray-300 dark:focus:border-blue-700 dark:active:bg-gray-700 dark:active:text-gray-300">
-                {!! __('pagination.next') !!}
+            <a href="{{ $paginator->nextPageUrl() }}" rel="next" class="relative inline-flex items-center px-4 py-2 text-sm font-medium text-black  leading-5 rounded-md hover:text-gray-500 focus:outline-none  active:text-gray-700 transition ease-in-out duration-150 ">
+                {!! __('Nächste') !!}
             </a>
         @else
-            <span class="relative inline-flex items-center px-4 py-2 text-sm font-medium text-gray-500 bg-white border border-gray-300 cursor-default leading-5 rounded-md dark:text-gray-600 dark:bg-gray-800 dark:border-gray-600">
-                {!! __('pagination.next') !!}
+            <span class="relative inline-flex items-center px-4 py-2 text-sm font-medium text-gray-500 cursor-default leading-5">
+                {!! __('Nächste') !!}
             </span>
         @endif
     </nav>
