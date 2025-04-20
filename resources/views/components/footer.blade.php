a029dc2fbe6c9b6e505899e68c96a84374e8541e
noah-swf <kingsleagueyt@gmail.com>
2025-04-20 19:42:40 +0200

    Refactor footer component, Add redirect for buttons on the landing page

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,4 +1,8 @@
-<div class="bg-neutral px-4 sm:px-6 lg:px-14 text-sm mt-20 md:mt-0">
+@php
+    $classes = "bg-neutral px-4 sm:px-6 lg:px-14 text-sm mt-20 md:mt-0"
+@endphp
+
+<div {{ $attributes->merge(['class' => $classes]) }}>
     <div class="h-0.5 bg-black/30 w-full my-2"></div>
     <div class="flex max-md:flex-col justify-between items-center py-6">
         <span class="text-gray-600">Built with <i class="fab fa-laravel"></i> Laravel by <a href="https://github.com/noah-swf" class="text-primary-600 font-semibold hover:underline underline-offset-4 ease-in-out duration-150">Noah</a>.</span>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -27,51 +27,64 @@
                             </a>
                         </div>
                         <!-- Account -->
-                        <div class="flex items-center ms-6">
-                            <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 hover:text-primary focus:outline-none transition ease-in-out duration-150">
-                                <div class="transition duration-150 ease-in-out">
-                                    <svg class="fill-current" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" >
-                                        <path d="M480-480q-66 0-113-47t-47-113q0-66 47-113t113-47q66 0 113 47t47 113q0 66-47 113t-113 47ZM160-160v-112q0-34 17.5-62.5T224-378q62-31 126-46.5T480-440q66 0 130 15.5T736-378q29 15 46.5 43.5T800-272v112H160Zm80-80h480v-32q0-11-5.5-20T700-306q-54-27-109-40.5T480-360q-56 0-111 13.5T260-306q-9 5-14.5 14t-5.5 20v32Zm240-320q33 0 56.5-23.5T560-640q0-33-23.5-56.5T480-720q-33 0-56.5 23.5T400-640q0 33 23.5 56.5T480-560Zm0-80Zm0 400Z"/>
-                                    </svg>
-                                </div>
-                            </button>
-                        </div>
+                        <form action="{{ route('profile.edit') }}" method="GET">
+
+
+                            <div class="flex items-center ms-6">
+                                <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 hover:text-primary focus:outline-none transition ease-in-out duration-150">
+                                    <div class="transition duration-150 ease-in-out">
+                                        <svg class="fill-current" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" >
+                                            <path d="M480-480q-66 0-113-47t-47-113q0-66 47-113t113-47q66 0 113 47t47 113q0 66-47 113t-113 47ZM160-160v-112q0-34 17.5-62.5T224-378q62-31 126-46.5T480-440q66 0 130 15.5T736-378q29 15 46.5 43.5T800-272v112H160Zm80-80h480v-32q0-11-5.5-20T700-306q-54-27-109-40.5T480-360q-56 0-111 13.5T260-306q-9 5-14.5 14t-5.5 20v32Zm240-320q33 0 56.5-23.5T560-640q0-33-23.5-56.5T480-720q-33 0-56.5 23.5T400-640q0 33 23.5 56.5T480-560Zm0-80Zm0 400Z"/>
+                                        </svg>
+                                    </div>
+                                </button>
+                            </div>
+                        </form>
                     </div>
                 </div>
             </nav>
-            <div class="relative mx-auto px-4 sm:px-6 lg:px-14 mt-8 lg:mt-16 h-[calc(100vh-11rem)] overflow-hidden">
+            <div class="relative mx-auto px-4 sm:px-6 lg:px-14 mt-8 lg:mt-16 lg:h-[calc(100vh-11rem)] h-[calc(100vh-5rem)] overflow-hidden">
                 <!-- Mockup -->
                 <div class="absolute bottom-0 right-0 pointer-events-none">
                   <img
                     src="{{ Vite::asset('resources/images/device-mockup.png') }}"
                     alt="latr app mockup"
-                    class="w-[75vw] lg:w-auto max-w-full"
+                    class="lg:w-auto max-w-full"
                     onerror="this.src='https://placehold.co/600x400?text=latr+App+Mockup'"
                   />
                 </div>
 
                 <!-- Content -->
-                <div class="lg:-mt-10 flex flex-col lg:flex-row items-center justify-between h-full relative z-10">
-                  <div class="lg:w-1/2 ">
+                <div
+                  class="flex flex-col items-center justify-center -mt-10
+                         lg:flex-row lg:justify-between
+                         h-full relative z-10"
+                >
+                  <div class="w-full lg:w-1/2 px-4 lg:px-0">  <!-- auf Mobile volle Breite mit Padding -->
                     <h1 class="font-lora italic text-4xl md:text-5xl leading-tight mb-6">
-                        Behalte deine Watchlist im Griff – immer und überall.
+                      Behalte deine Watchlist im Griff – immer und überall.
                     </h1>
                     <p class="text-lg mb-8 text-gray-700">
                       Organisiere Videos, Filme und Serien an einem Ort und schaue sie, wenn du Zeit hast.
                     </p>
-                    <x-primary-button>
-                      Jetzt loslegen
-                      <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 ml-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14 5l7 7m0 0l-7 7m7-7H3" />
-                      </svg>
-                    </x-primary-button>
+                    <form action="{{ route('home') }}" method="GET">
+                        @csrf
+                        <x-primary-button>
+                            Jetzt loslegen
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 ml-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14 5l7 7m0 0l-7 7m7-7H3" />
+                            </svg>
+                          </x-primary-button>
+                    </form>
+
                   </div>
                 </div>
               </div>
 
 
 
+
         </div>
-        <x-footer/>
+        <x-footer class="mt-0"/>
     </body>
 </html>
