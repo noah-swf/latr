e0b9c44fa53ded0231349e167e51e299f4f4f526
noah-swf <kingsleagueyt@gmail.com>
2025-04-21 20:16:38 +0200

    Add empty state messages for video lists in home and watched pages

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,7 +13,13 @@
         <div class="mt-5 right-0">
             {{ $videos->links() }}
         </div>
+        @if ($videos->isEmpty())
+            <div class="w-full py-32">
+                <h2 class="text-xl font-bold text-gray-700 opacity-50">Deine Liste ist noch leer.</h2>
+                <p class="text-gray-500 mt-1 opacity-50">  Füge spannende Videos hinzu, um sie später hier anzusehen!</p>
+            </div>
 
+        @endif
 
     </div>
 </x-app-layout>
--- resources/views/watched.blade.php
+++ resources/views/watched.blade.php
@@ -9,4 +9,12 @@
             {{ $videos->links() }}
         </div>
     </div>
+    <div class="w-11/12 md:w-4/5 lg:w-3/4 lg:h-[calc(100vh-11rem)] h-[calc(100vh-5rem)] mx-auto text-center flex flex-col items-center justify-center">
+        @if ($videos->isEmpty())
+        <div class="w-full">
+            <h2 class="text-2xl font-bold text-gray-700">Noch keine Videos angesehen?</h2>
+            <p class="text-gray-500 mt-2"> Hake Videos von deiner Watch-Later-Liste ab, und du findest sie hier jederzeit wieder.</p>
+        </div>
+    @endif
+    </div>
 </x-app-layout>
