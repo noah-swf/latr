74bb1f1e04c200f4b4668eb4c9e227f51fc0ac19
noah-swf <kingsleagueyt@gmail.com>
2025-04-24 18:45:05 +0200

    Move latr logo

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,5 +1,5 @@
 @vite(['resources/css/app.css', 'resources/js/app.js'])
 
-<img src="{{ Vite::asset('resources/images/latr-logo.png') }}" {{ $attributes->merge(['class' => 'h-20']) }} alt="Logo" />
+<img src="{{ Vite::asset('images/latr-logo.png') }}" {{ $attributes->merge(['class' => 'h-20']) }} alt="Logo" />
 
 
