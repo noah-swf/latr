f9bb3ea4e9c3d90927db82d2ad58733920141508
noah-swf <kingsleagueyt@gmail.com>
2025-04-25 18:36:23 +0200

    Readded images to resources folder

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,5 +1,5 @@
 @vite(['resources/css/app.css', 'resources/js/app.js'])
 
-<img src="{{ asset('images/latr-logo.png') }}" {{ $attributes->merge(['class' => 'h-20']) }} alt="Logo" />
+<img src="{{ Vite::asset('resources/images/latr-logo.png') }}" {{ $attributes->merge(['class' => 'h-20']) }} alt="Logo" />
 
 
